9abd3c2a53d9b704e9fb411bf8a5e0d178495500
SantiagoLGutnfra <sleonel791@gmail.com>
2023-01-30 12:45:22 -0300

    guia 1 trabajos 1 y 2

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -3,6 +3,17 @@ Debemos lograr tomar un nombre con 'prompt'
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	alert("ok");
-}
+	//alert("ok");
+	/* va a ser para guardarme comentarios para mi o para alguien que lo vea */
 
+    // var edad;
+	let nombre;
+    let nombreCompleto; // motacion lower camel case
+
+	nombre = prompt("ingrese un nombre");
+	// esto es con prompt pero no tengo idea que es.
+	// nombre = "santiago"; //asignacion de string
+
+	// nombre = null;
+	alert(nombre)
+}
